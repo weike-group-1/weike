6c7cbca897fa37568e24a4987571fcbfbe1c8c3e
qingzhizhu10086 <33675166+qingzhizhu10086@users.noreply.github.com>
2017-12-13 14:15:12 +0800

    baseService

---
+++ src/cn/ucai/weike/service/BaseService.java
@@ -0,0 +1,20 @@
+package cn.ucai.weike.service;
+
+import java.io.Serializable;
+
+import cn.ucai.weike.utils.Result;
+
+public interface BaseService<T> {
+
+    Result addEntity(T t);
+	
+    Result deleteEntity(T t);
+	
+    Result updateEntity(T t);
+	
+	Result getEntity(Serializable id);
+	
+	Result getAllEntityByPage(Integer pageNo,Integer pageSize,Object...objects);
+	
+	Result getEntityCount();
+}
